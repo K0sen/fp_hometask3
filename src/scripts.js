b7f909c07bddace971c8fb9c93cadb732861e82f
k0sen <caladien12@gmail.com>
2018-12-02 22:27:26 +0200

    init

---
+++ src/scripts.js
@@ -0,0 +1,8 @@
+const { Map } = Immutable;
+
+const map1 = Map({ a: 1, b: 2, c: 3 });
+const map2 = map1.set('b', 50);
+map1.get('b') + " vs. " + map2.get('b'); // 2 vs. 50
+
+console.log(map1.get('b') + " vs. " + map2.get('b'));
+console.log(Immutable);
